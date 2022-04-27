38b769225472e535ab3f41fe4d0c260f9e334f5b
57383 <jasonvinda@gmail.com>
2022-04-27 11:55:15 +0200

    button filter

--- script.js
+++ script.js
@@ -6,3 +6,18 @@ const menu = [
         type: '30+ min.',
     },
 ]
+const form = document.createElement("form");
+const label = document.createElement('label')
+const input = document.createElement('input')
+const button = document.createElement('button')
+label.setAttribute("for", "search")
+input.setAttribute("type", "text")
+input.setAttribute("id", "search")
+button.textContent = 'search'
+
+document.body.appendChild(form)
+
+form.appendChild(label)
+form.appendChild(input)
+form.appendChild(button)
+
